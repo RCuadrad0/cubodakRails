4941ebc0fcd00fc66a491d1a32bc1cdd6198a24d
egromero <egromero@uc.cl>
2021-05-22 18:50:15 -0400

    add frontend

--- cubodak/app/helpers/application_helper.rb
+++ cubodak/app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if !!session[:user_id]
     end
+
+    def zondicon(icon_name, options={})
+        file = File.read(Rails.root.join('app', 'assets', 'images', "#{icon_name}.svg"))
+        doc = Nokogiri::HTML::DocumentFragment.parse file
+        svg = doc.at_css 'svg'
+        options.each {|attr, value| svg[attr.to_s] = value}
+        doc.to_html.html_safe
+  end
+
+
 end
