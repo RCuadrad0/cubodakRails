aeabbdc25915a01dacb74069c5fb2a94d6e7fdb1
egromero <egromero@uc.cl>
2021-05-21 23:07:33 -0400

    siginup logica lista

--- cubodak/config/routes.rb
+++ cubodak/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'landing#index'
+  resources :users
+
+  get '/auth/signin', to: 'sessions#new'
+  post '/auth/signin', to: 'sessions#create'
+  post '/auth/signout', to: 'sessions#destroy'
+  get '/auth/siginout', to: 'sessions#destroy'
+  get '/auth/signup', to: 'users#new'
+  post '/auth/signup', to: 'users#create'
+
 end
